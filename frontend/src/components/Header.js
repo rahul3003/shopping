fabdfae76c83a96a1c68b2705c08e52a86fd0094
rahul@3003 <rahulkhandke71@gmail.com>
2023-04-08 17:00:09 +0530

    Header completed

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { NavLink, Link } from 'react-router-dom'
+import { BsSearch } from 'react-icons/bs'
 
 const Header = () => {
   return (
@@ -22,15 +23,77 @@ const Header = () => {
       </header>
       <header className='header-upper py-3'>
         <div className='container-xxl'>
-          <div class="row">
+          <div class="row align-items-center">
             <div className='col-2'>
               <h2><Link className='text-white'>Shoppy.</Link></h2>
             </div>
             <div className='col-5'>
-              
+              <div class="input-group">
+                <input type="text" class="form-control py-2" placeholder="Search Product Here..." aria-label="Search Product Here..." aria-describedby="basic-addon2" />
+                <span class="input-group-text px-3" id="basic-addon2"><BsSearch className='fs-6' /></span>
+              </div>
             </div>
             <div className='col-5'>
-              
+              <div className='header-upper-links d-flex align-content-center justify-content-between'>
+                <div>
+                  <Link className='d-flex align-items-center gap-10 text-white'>
+                    <img src="/images/compare.svg" alt="compare" />
+                    <p className='mb-0'>Compare <br /> Product</p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className='d-flex align-items-center gap-10 text-white'>
+                    <img src="/images/wishlist.svg" alt="wishlist" />
+                    <p className='mb-0'>Favorite <br /> Wishlist</p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className='d-flex align-items-center gap-10 text-white'>
+                    <img src="/images/user.svg" alt="login" />
+                    <p className='mb-0'>Login <br /> My Account</p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className='d-flex align-items-center gap-10 text-white'>
+                    <img src="/images/cart.svg" alt="cart" />
+                    <div className='d-flex flex-column gap-10'>
+                      <span className='badge bg-white text-dark'>0</span>
+                      <p className='mb-0'>$ 500</p>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <header className='header-bottom py-3'>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='col-12 m-0'>
+              <div className='menu-bottom d-flex align-items-center gap-30'>
+                <div>
+                  <div className="dropdown">
+                    <button className="btn btn-secondary dropdown-toggle bg-transparent border-0 d-flex gap-15 align-items-center" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                      <img src="/images/menu.svg" alt="menu"/>
+                      <span className='me-5 d-inline-block'>Shop Categories</span> 
+                    </button>
+                    <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                      <li><Link className="dropdown-item text-white" to="#">Action</Link></li>
+                      <li><Link className="dropdown-item text-white" to="#">Another action</Link></li>
+                      <li><Link className="dropdown-item text-white" to="#">Something else here</Link></li>
+                    </ul>
+                  </div>
+                </div>
+                <div className='menu-links'>
+                  <div className='d-flex align-items-center gap-15'>
+                    <NavLink className='text-white' to='/home'>Home</NavLink>
+                    <NavLink className='text-white' to='/home'>Our Store</NavLink>
+                    <NavLink className='text-white' to='/home'>Blogs</NavLink>
+                    <NavLink className='text-white' to='/home'>Contact</NavLink>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
