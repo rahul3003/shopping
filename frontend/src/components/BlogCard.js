44b3e8e8fea4fed267d80fcc5fb5ae5d5f774f5a
rahul@3003 <rahulkhandke71@gmail.com>
2023-05-06 19:28:33 +0530

    blog card

---
+++ frontend/src/components/BlogCard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const BlogCard = () => {
+    return (
+        <>
+        <div className='col-3'>
+            <div className='blog-card'>
+                <div className='blog-image'>
+                    <img src="images/blog-1.jpg" alt="blogs"/>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default BlogCard
