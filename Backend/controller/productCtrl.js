7811cec8535b21009fb267a81ec0bdeafa9d26f3
rahul@3003 <rahulkhandke71@gmail.com>
2023-05-16 00:02:55 +0530

    product model created

---
+++ Backend/controller/productCtrl.js
@@ -0,0 +1,11 @@
+const Product = require('../modules/productModel.js')
+const asyncHandler = require('express-async-handler')
+
+
+const createProduct = asyncHandler(async (req, res) => {
+
+})
+
+module.exports = {
+    createProduct
+}
