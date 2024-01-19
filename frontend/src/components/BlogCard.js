adc1b498c781e45474785304f12e54841feb87e6
rahul3003 <rahulkhandke71@gmail.com>
2024-01-19 19:42:11 +0530

    new changes

--- frontend/src/components/BlogCard.js
+++ frontend/src/components/BlogCard.js
@@ -6,7 +6,7 @@ const BlogCard = () => {
         <div className='col-3'>
             <div className='blog-card'>
                 <div className='blog-image'>
-                    <img src="images/blog-1.jpg" alt="blogs"/>
+                    <img src="images/blog-1.jpg" className='img-fluid' alt="blogs"/>
                 </div>
             </div>
         </div>
