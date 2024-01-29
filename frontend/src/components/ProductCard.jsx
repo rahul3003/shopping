91757e67ab915bbb0174f55680c398dbfb6b2ec3
rahul3003 <rahulkhandke71@gmail.com>
2024-01-29 14:01:28 +0530

    product card completed

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import ReactStars from "react-rating-stars-component";
+import { Link } from 'react-router-dom';
 
 const ProductCard = () => {
 
     return (
         <>
             <div className='col-3'>
-                <div className='product-card'>
+                <div className='product-card position-relative'>
+                    <div className='wishlist-icon position-absolute'>
+                        <Link>
+                            <img src="images/wish.svg" alt="add-cart" />
+                        </Link>
+                    </div>
                     <div className='product-image'>
-                        <img src="images/watch.jpg" alt="product image" />
+                        <img src="images/watch.jpg" alt="product-image" />
+                        <img src="images/watch-1.webp" style={{width:"70%",margin:"auto"}} alt="product-image" />
+                    </div>
+                    <div>
                         <div className="product-details">
                             <h6 className="brand">Havels</h6>
                             <h5 className="product-title">
@@ -18,12 +27,25 @@ const ProductCard = () => {
                                 count={5}
                                 size={24}
                                 value={3}
-                                edit={"false"}
+                                edit={false}
                                 activeColor="#ffd700"
                             />
                             <p className="price">
                                 $100.00
                             </p>
+                        </div>
+                    </div>
+                    <div className="action-bar position-absolute">
+                        <div className='d-flex flex-column flex-column-reverse gap-2'>
+                            <Link>
+                                <img src="images/prodcompare.svg" alt="compare" />
+                            </Link>
+                            <Link>
+                                <img src="images/view.svg" alt="add-cart" />
+                            </Link>
+                            <Link>
+                                <img src="images/add-cart.svg" alt="add-cart" />
+                            </Link>
                         </div>
                     </div>
                 </div>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -172,6 +172,19 @@ const Home = () => {
           </div>
         </div>
       </section>
+      <section className='featured wrapper py-5 home-wrapper-2'>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='col-12'>
+              <h3 className='section-heading'>Featured Collection</h3>
+            </div>
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+          </div>
+        </div>
+      </section>
       <section className='marquee-wrapper py-5'>
         <div className='container-xxl'>
           <div className='row'>
@@ -202,19 +215,6 @@ const Home = () => {
                 </Marquee>
               </div>
             </div>
-          </div>
-        </div>
-      </section>
-      <section className='blog wrapper py-5 home-wrapper-2'>
-        <div className='container-xxl'>
-          <div className='row'>
-            <div className='col-12'>
-              <h3 className='section-heading'>Featured Collection</h3>
-            </div>
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
           </div>
         </div>
       </section>
